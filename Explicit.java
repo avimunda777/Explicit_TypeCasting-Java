3f42b09aa9accdae625929dd49797ca5cfaa8837
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2025-05-22 00:00:53 +0530

    Add files via upload

---
+++ Explicit.java
@@ -0,0 +1,10 @@
+                    // Explicit TypeCasting
+class explicit
+{
+    public static void main(String[] args)
+    {
+        double a = 10.5; // 8 Bytes
+        int b = (int)a; // 4 Bytes
+        System.out.println(b);
+    }
+}
